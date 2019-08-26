4a0bcedb36074595b7cd5a0fbb033a3214d2437e
WilsonWu <wilson.wu@kantar.com>
2019-08-26 19:19:38 +0800

    完成前端初步模型

---
+++ static/js/calculator.js
@@ -0,0 +1,4 @@
+const remainder = document.getElementById("remainder");
+const root = document.getElementById("root");
+const sndPower = document.getElementById("sndPower");
+const reciprocal = document.getElementById("reciprocal");
